e15711865fec07bbd3fd0aa49caf3a9a8a033909
tadanobu.hosaka <tomoyaaaa.go@gmail.com>
2018-06-21 09:16:43 +0000

    user sign&login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'tasks#index'
+
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   resources :tasks
 end
